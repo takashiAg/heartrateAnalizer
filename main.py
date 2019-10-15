9b15d4a1b77abeda5ad04601c132b169350a8079
takashiAg <crawd4274@gmail.com>
2019-10-15 13:13:37 +0900

    add draw function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import csv
 import numpy as np
+import matplotlib.pyplot as plt
 
 filterCoefficient = 0.99
 
@@ -9,6 +10,20 @@ def readCsv(fileName):
         reader = csv.reader(f)
         header = next(reader)
         return [[row[0], float(row[1])] for row in reader]
+
+
+def drawData(label, data):
+    fig = plt.figure()
+
+    # 図の中にサブプロットを追加する
+    subPlots = []
+    for d in range(len(data)):
+        subPlots.append(fig.add_subplot(len(data), 1, d + 1))
+
+    for i, subPlot in enumerate(subPlots):
+        fig1_a_1, = subPlot.plot(label, data[i])
+
+    plt.show()
 
 
 def main():
@@ -31,6 +46,11 @@ def main():
     print(len(time), len(heartRate), len(filteredHeartRate))
 
     print(time, heartRate, filteredHeartRate)
+    x = np.linspace(-2 * np.pi, 2 * np.pi, 10000)
+    y1 = np.sin(x)
+    y2 = np.cos(x)
+    y3 = np.tan(x)
+    drawData(x, [y1, y2, y3])
 
 
 if __name__ == '__main__':
