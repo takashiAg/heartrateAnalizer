8a131cb462d5d03d2680ff74e7615a9f7d3b0a18
takashiAg <crawd4274@gmail.com>
2019-10-15 12:10:29 +0900

    add read csv funciton

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
 import csv
 
+
+def readCsv(fileName):
+    with open(fileName) as f:
+        reader = csv.reader(f)
+        return [row for row in reader]
+
+
 def main():
-    print("start")
+    data = readCsv("rawData20191014.csv")
+    print(data)
+
 
 if __name__ == '__main__':
     main()
