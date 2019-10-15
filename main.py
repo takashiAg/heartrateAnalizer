2762978ac778b91071fc83a968458c6ef9d9bce9
takashiAg <crawd4274@gmail.com>
2019-10-15 12:54:41 +0900

    add filter function

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import csv
 import numpy as np
 
+filterCoefficient = 0.99
+
 
 def readCsv(fileName):
     with open(fileName) as f:
         reader = csv.reader(f)
         header = next(reader)
-        return [[row[0], int(row[1])] for row in reader]
+        return [[row[0], float(row[1])] for row in reader]
 
 
 def main():
@@ -15,6 +17,17 @@ def main():
 
     # 転地してtimeとheartrateに分割
     (time, heartRate) = np.array(data).T
+
+    filteredHeartRate = []
+    print(0.0 * filterCoefficient)
+    for HR in heartRate:
+        filteredHeartRate.append(
+            HR if len(filteredHeartRate) == 0
+            else float(filteredHeartRate[-1]) * filterCoefficient
+                 + float(HR) * (1.0 - filterCoefficient)
+        )
+
+    print(len(time), len(heartRate))
 
     print(time, heartRate)
 
