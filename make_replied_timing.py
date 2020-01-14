006c0a7751f2fcb041f383814e80d5ad9c800eb1
takashiAg <crawd4274@gmail.com>
2020-01-15 05:56:51 +0900

    fix for happyou

---
+++ make_replied_timing.py
@@ -0,0 +1,21 @@
+import re
+import csv
+import datetime
+
+fileName = "justtiming_takatoshi_2.csv"
+
+baseTime = datetime.time(hour=14, minute=40)
+timeStep = datetime.timedelta(minutes=5)
+count = 48
+
+startTime = datetime.datetime.combine(datetime.date.today(), baseTime)
+
+f = open(fileName, 'w')
+writer = csv.writer(f)
+writer.writerow(["time"])
+for i in range(count):
+    writer.writerow([(startTime + i * timeStep).strftime("%H:%M")])
+f.close()
+
+if __name__ == '__main__':
+    pass
