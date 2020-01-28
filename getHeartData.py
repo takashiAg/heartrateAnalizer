b6e4a005fe081a71e4c89f190bba90c9f8cad506
takashiAg <crawd4274@gmail.com>
2020-01-28 15:31:08 +0900

    add arisa data

--- getHeartData.py
+++ getHeartData.py
@@ -5,13 +5,14 @@ import csv
 
 id = 18717
 
-year = 2019
-month = 12
-day = 10
+year = 2020
+month = 1
+day = 28
 
 # url = 'http://localhost:8080/api/heartrate/18717/2019/12/11'
-url = 'http://localhost:8080/api/heartrate/{id}/{year}/{month}/{day}' \
-    .format(id=id, year=year, month=month, day=day)
+url = 'http://131.113.137.5:3000/api/heartrate/{id}/{startTimestamp:.0f}/{stopTimestamp:.0f}' \
+    .format(id=id, startTimestamp=datetime.datetime(year, month, day, 0, 0, 0, 0).timestamp() * 1000,
+            stopTimestamp=datetime.datetime(year, month, day + 1, 0, 0, 0, 0).timestamp() * 1000)
 
 
 def get():
@@ -29,7 +30,7 @@ def main():
         dt_jst_aware = datetime.datetime.fromtimestamp(d["timestamp"] / 1000,
                                                        datetime.timezone(datetime.timedelta(hours=9)))
         rawData.append({"time": "{0:%H:%M:%S}".format(dt_jst_aware), "heartRate": d["ComputedHeartRate"]})
-    print(rawData)
+    # print(rawData)
     f = open('data_{year}_{month}_{day}.csv'
              .format(year=year, month=month, day=day), 'w')
     writer = csv.writer(f)
