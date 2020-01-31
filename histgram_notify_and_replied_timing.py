2dd095a31c5a854707ffe980beb2ac234f5b2320
takashiAg <crawd4274@gmail.com>
2020-01-31 12:41:08 +0900

    add scatter

--- histgram_notify_and_replied_timing.py
+++ histgram_notify_and_replied_timing.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-username = "nakano"
+username = "arisa"
 filepath = "data/{username}/{filename}.csv"
 
 
@@ -15,14 +15,16 @@ def readCsv(fileName):
 
 def main():
     notified_replied_timing = readCsv(filepath.format(username=username, filename="notified_replied_timing"))
+    # notified_replied_timing = [row for row in notified_replied_timing if row[0] != 0]
     (time_respond, JIT_notify, JIT_respond) = np.array(notified_replied_timing).T
     print(notified_replied_timing)
 
     fig = plt.figure()
 
     ax = fig.add_subplot(1, 1, 1)
-    ax.scatter(time_respond, JIT_notify, c='red',label="JIT index at notified")
-    ax.scatter(time_respond, JIT_respond, c='blue',label="JIT index at respond")
+
+    ax.scatter(time_respond, JIT_respond, c='none', linewidths=1, edgecolors="red", label="JIT index at respond")
+    ax.scatter(time_respond, JIT_notify, marker='x', label="JIT index at notified")
 
     # ax.set_title(' ')
     ax.set_xlabel('time to respond [min]')
