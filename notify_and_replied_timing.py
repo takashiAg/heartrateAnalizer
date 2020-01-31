e4700effa9c7de8573aa75844e05c31d525bf7ab
takashiAg <crawd4274@gmail.com>
2020-01-31 11:55:03 +0900

    add JIT index csv

--- notify_and_replied_timing.py
+++ notify_and_replied_timing.py
@@ -12,6 +12,14 @@ def readCsv(fileName):
         return [[dt.strptime(row[0], '%H:%M')] for row in reader]
 
 
+def readJITindex(username):
+    fileName = "data/{username}/JITindex.csv".format(username=username)
+    with open(fileName) as f:
+        reader = csv.reader(f)
+        header = next(reader)
+        return [[dt.strptime(row[0], '%H:%M'), float(row[1])] for row in reader]
+
+
 def writeCsv(fileName, data, header):
     with open(fileName, mode='w') as f:
         writer = csv.writer(f)
@@ -25,21 +33,38 @@ def main():
     replied_timing = readCsv(filepath.format(username=username, filename="replied_timing"))
     print(notified_timing, replied_timing)
 
+    JITindex = readJITindex(username)
+    print("JIT index")
+    print(JITindex)
+
     data = []
     pointer = 0
+    jit_pointer = 0
+    jit_pointer2 = 0
     for n in notified_timing:
         while (replied_timing[pointer][0] < n[0]):
             if len(replied_timing) - 1 <= pointer:
                 break
             pointer += 1
+
+        while (JITindex[jit_pointer][0] < n[0]):
+            if len(JITindex) - 1 <= jit_pointer:
+                break
+            jit_pointer += 1
+
+        while (JITindex[jit_pointer2][0] < replied_timing[pointer][0]):
+            if len(JITindex) - 1 <= jit_pointer2:
+                break
+            jit_pointer2 += 1
         diff = (replied_timing[pointer][0] - n[0]).total_seconds() / 60
         diff = int(diff) if diff >= 0 else 0
-        data.append([n[0].strftime("%H:%M"), replied_timing[pointer][0].strftime("%H:%M"), diff])
+        data.append([n[0].strftime("%H:%M"), replied_timing[pointer][0].strftime("%H:%M"), diff,
+                     int(JITindex[jit_pointer][1]), int(JITindex[jit_pointer2][1])])
 
     writeCsv(
         filepath.format(username=username, filename="notified_replied_timing"),
         data,
-        ["notified timing", "replied timing", "time to respond [min]"]
+        ["notified timing", "replied timing", "time to respond [min]", "JIt index at notified", "JIT index at replied"]
     )
 
 
